639e00f4e7eef13a092bd4f84fe4665201a79ad5
Ikechukwu-attah <attahikechukwu@gmail.com>
2023-08-06 16:24:29 -0400

    initial commit

--- controllers/summarization.js
+++ controllers/summarization.js
@@ -3,95 +3,80 @@ import { extractArticleContent } from "./webScraping.js";
 import SummarizedArticleModel from "../models/summarizedArticle.js";
 import mongoose from "mongoose";
 
-// export const handleSummarization = async (req, res, url) => {
-//   try {
-//     const text = req.body.text;
-//     // console.log("text::", text);
-//     const summary = await summarizeArticle(text);
-//     console.log("Summary:::", summary);
-//     res.json({ summary, url });
-//     const result = new SummarizedArticleModel({ summary, url });
-//     const saveResult = result.save();
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: "Failed to summarize text" });
-//   }
-// };
+export const handleSummarization = async(req, res, url) => {
+    try {
+        const text = req.body.text;
+        const summary = await summarizeArticle(text);
 
-export const handleSummarization = async (req, res, url) => {
-  try {
-    const text = req.body.text;
-    const summary = await summarizeArticle(text);
+        const newArticle = new SummarizedArticleModel({ summary, url });
+        await newArticle.save();
 
-    const result = new SummarizedArticleModel({ summary, url });
-    await result.save();
-
-    res.json({ summary, url });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Failed to summarize text" });
-  }
-};
-
-export const summarized = async (req, res) => {
-  try {
-    const url = req.body.url;
-    // console.log("url::", url);
-    const articleContent = await extractArticleContent(url);
-    // console.log("articleContent:", articleContent);
-    req.body.text = articleContent;
-    await handleSummarization(req, res, url);
-  } catch (error) {
-    console.log("error from summary", error);
-    res.status(500).json({ error: "Failed to summarize text" });
-  }
-};
-
-export const getAllSummarizedArticle = async (req, res) => {
-  try {
-    const result = await SummarizedArticleModel.find();
-    res.status(200).json(result);
-  } catch (error) {
-    console.log({ error });
-    res.status(500).json({ error: error });
-  }
-};
-
-export const getSingleSummarizedArticle = async (req, res) => {
-  const { articleId } = req.params;
-  console.log({ articleId });
-  const article = await SummarizedArticleModel.findById(articleId);
-  try {
-    if (!article) {
-      res.status(404).json({ message: "Article not found" });
-    } else {
-      res.status(200).json(article);
+        res.json(newArticle);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Failed to summarize text" });
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
 };
 
-export const deleteSummarizedArticle = async (req, res) => {
-  const { articleId } = req.params;
-  console.log({ articleId });
+export const summarized = async(req, res) => {
+    try {
+        const url = req.body.url;
+        // console.log("url::", url);
+        const articleContent = await extractArticleContent(url);
+        // console.log("articleContent:", articleContent);
+        req.body.text = articleContent;
+        await handleSummarization(req, res, url);
+    } catch (error) {
+        console.log("error from summary", error);
+        res.status(500).json({ error: "Failed to summarize text" });
+    }
+};
 
-  if (!mongoose.Types.ObjectId.isValid(articleId)) {
-    return res.status(404).json({ message: "Invalid article id" });
-  }
+export const getAllSummarizedArticle = async(req, res) => {
+    try {
+        const result = await SummarizedArticleModel.find();
+        res.status(200).json(result);
+    } catch (error) {
+        console.log({ error });
+        res.status(500).json({ error: error });
+    }
+};
 
-  try {
-    const deletedArticle = await SummarizedArticleModel.findByIdAndDelete(
-      articleId
-    );
+export const getSingleSummarizedArticle = async(req, res) => {
+    const { articleId } = req.params;
+    console.log({ articleId });
+    const article = await SummarizedArticleModel.findById(articleId);
+    try {
+        if (!article) {
+            res.status(404).json({ message: "Article not found" });
+        } else {
+            res.status(200).json(article);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
 
-    if (!deletedArticle) {
-      return res.status(404).json({ message: "No article with this id found" });
+export const deleteSummarizedArticle = async(req, res) => {
+    const { articleId } = req.params;
+    console.log({ articleId });
+
+    if (!mongoose.Types.ObjectId.isValid(articleId)) {
+        return res.status(404).json({ message: "Invalid article id" });
     }
 
-    return res.status(200).json({ message: "Item deleted successfully" });
-  } catch (error) {
-    return res.status(500).json({ error: "Something went wrong" });
-  }
+    try {
+        const deletedArticle = await SummarizedArticleModel.findByIdAndDelete(
+            articleId
+        );
+
+        if (!deletedArticle) {
+            return res.status(404).json({ message: "No article with this id found" });
+        }
+
+        return res.status(200).json({ message: "Item deleted successfully" });
+    } catch (error) {
+        return res.status(500).json({ error: "Something went wrong" });
+    }
 };
